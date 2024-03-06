9ce2a68ab19f2a4b08b80aa832be0f9362456ac7
akit <149289426+Sstudiosdev@users.noreply.github.com>
2024-03-06 15:52:19 -0600

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,16 @@
+@echo off
+
+rem Check if Node.js is installed
+node -v > nul 2>&1
+if errorlevel 1 (
+    echo Node.js is not installed. Please install Node.js before running this file.
+    echo Closing in 15 seconds...
+    for /l %%i in (15,-1,1) do (
+        echo Closing in %%i seconds...
+        ping -n 2 127.0.0.1 > nul
+    )
+    exit
+)
+
+rem Run the file
 node .
